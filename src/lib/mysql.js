27b4e8b54fe5a5c538554293c06cb7300430225f
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-11-15 20:45:02 +0100

    Update mysql.js

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise';
 
-const DATABASE_URL='mysql://root:root@localhost:3306/productos'
+const DATABASE_URL='mysql://root:root@localhost:3306/tienda'
 
 const connection = await mysql.createConnection(DATABASE_URL)
 
